8808bcad84816459facc69d054fb6fe8c3873c9f
PDRX91 <parker.rebensdorf@gmail.com>
2022-05-03 15:24:36 -0700

    adding song to playlist is functional

--- bin/index.js
+++ bin/index.js
@@ -2,12 +2,15 @@
 const fs = require("fs");
 const INPUT_FILEPATH = `./${process.argv[2]}`;
 const CHANGES_FILEPATH = `./${process.argv[3]}`;
+const OUTPUT_FILEPATH = `./${process.argv[4]}`;
 const readJson = async () => {
   const inputData = await fs.promises.readFile(INPUT_FILEPATH, (err, data) => {
     try {
       return data;
     } catch {
-      throw new Error`There was an issue reading the file at ${INPUT_FILEPATH}`();
+      throw new Error(
+        `There was an issue reading the file at ${INPUT_FILEPATH}`
+      );
     }
   });
   const changeData = await fs.promises.readFile(
@@ -16,21 +19,42 @@ const readJson = async () => {
       try {
         return data;
       } catch {
-        throw new Error`There was an issue reading the file at ${CHANGES_FILEPATH}`();
+        throw new Error(
+          `There was an issue reading the file at ${CHANGES_FILEPATH}`
+        );
       }
     }
   );
-  // console.log(JSON.parse(inputData));
-  // console.log(JSON.parse(changeData));
+
   updateJson(inputData, changeData);
 };
 
 const updateJson = (initialData, changeData) => {
   const changes = JSON.parse(changeData);
   const input = JSON.parse(initialData);
-  console.log(changes);
-  console.log(Object.keys(changes));
+  // console.log(changes);
+  const keys = Object.keys(changes);
+  keys.forEach((key) => {
+    if (key === "add_songs_to_playlist") {
+      console.log(changes.add_songs_to_playlist);
+      console.log(input.playlists);
+      changes.add_songs_to_playlist.forEach((item) => {
+        const result = input.playlists.find(
+          (playlist) => playlist.id === item.playlist_id
+        );
+        if (result) {
+          result.song_ids.push(item.song_id);
+        } else {
+          throw new Error("Playlist not valid");
+        }
+        console.log("RESULT", input.playlists);
+      });
+    }
+
+    fs.writeFile(OUTPUT_FILEPATH, JSON.stringify(input, null, 2), (err) => {
+      if (err) console.log("Error writing file:", err);
+    });
+  });
 };
 
 readJson();
-// console.log("hello");
